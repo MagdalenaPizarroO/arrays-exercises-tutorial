3ae79562355eeb10677de18665bd6fcdda54ecfb
MagdalenaPizarroO <mspizarro.ortega@gmail.com>
2023-02-20 02:53:02 +0000

    hasta ejercicio 07.2

--- .learn/resets/04.4-Add_items_to_array_looping/app.js
+++ .learn/resets/04.4-Add_items_to_array_looping/app.js
@@ -4,7 +4,3 @@ let arr = [4,5,734,43,45];
 // Your code here
 // you need to loop 10 times, for example, using a for loop
 //*****************
-for (var i=0;i<10; i++) {
-arr.push(Math.floor(Math.random()*1000))
-}
-console.log(arr)
--- exercises/07.2-Letter-Counter/app.js
+++ exercises/07.2-Letter-Counter/app.js
@@ -2,5 +2,17 @@ let par = "Lorem ipsum dolor sit amet consectetur adipiscing elit Curabitur eget
 let counts = {};
 
 // your code here
-
+for (let i in par) {
+    const letter = par[i].toLowerCase(); 
+    //si hago console.log(letter), me va a separar todas las letras.
+    if(letter == " ") continue; //es decir, se salta los espacios
+    else if(counts[letter] == undefined){
+        counts[letter] = 1; // es decir, la primera vez que encuentra esa letra, le asigna el número 1
+    }
+    else{
+        counts[letter] = counts[letter] + 1; // cada vez que la vuelva a encontrar, le suma 1
+    }
+}
 console.log(counts);
+
+
