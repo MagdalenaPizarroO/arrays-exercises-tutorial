86c9e75af2cb12fb422066257c66c0236e803433
MagdalenaPizarroO <mspizarro.ortega@gmail.com>
2023-02-26 23:41:08 +0000

    voy en 09.1

--- exercises/08.1-Mixed-array/app.js
+++ exercises/08.1-Mixed-array/app.js
@@ -1,3 +1,20 @@
 let mix = [42, true, "towel", [2,1], 'hello', 34.4, {"name": "juan"}];
 
 //your code here
+
+//esto es lo que armé yo, también funciona!!
+let newArr = [];
+for (i = 0; i < mix.length; i++) {
+    newArr.push(typeof mix[i]);
+}
+console.log(newArr);
+
+
+//esta es la respuesta para que el test salga bien :(
+let newArray = [];
+for (let i = 0; i < mix.length; i++) {
+    const item = mix[i];
+    newArray.push(typeof item)
+    
+}
+console.log(newArray)
